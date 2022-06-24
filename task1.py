3be5418cf698e1a5ede188f27a3838c9dca4008f
2025raj <oliraj2025@gmail.com>
2022-06-24 22:55:17 +0545

    task 1 has been completed/ revised . But 3 questions are still remaining

---
+++ task1.py
@@ -0,0 +1,188 @@
+# question 1
+######################
+# a = 5
+# b = 6
+
+# if a == b:
+#     print(1)
+
+# elif a > b:
+#     print(2)
+# else:
+#     print(3)
+
+
+##########################################################################################################################################
+
+# question2
+###################
+
+
+# a = 20
+# b = 20
+# c = 30
+# d = 30
+# if a == b and c == d:
+#     print("hello")
+
+
+#######################################################################################################################################
+
+# question3
+###############
+
+
+# a = 10
+# b = 10
+# c = 30
+# d = 40
+
+# if a == b or c == d:
+#     print("hello")
+
+####################################################################################################################################
+
+# question4
+#################
+
+
+# x = int(input("enter a number: "))
+
+# if x > 0:
+#     print("True")
+
+# elif x < 0:
+#     print("False")
+
+# else:
+#     print("zero")
+
+#######################################################################################################################################
+
+# question 5
+#################
+
+# num = int(input("enter a number: "))
+# if num >= 0:
+#     if num % 2 == 0:
+#         print("Given input is even number")
+#     else:
+#         print("Given input is odd number")
+
+# else:
+#     print("please check your input")
+
+#############################################################################################################################################
+
+# question6
+#################
+
+
+# subject_1 = float(input("enter a first subject marks: "))
+# subject_2 = float(input("enter a second subject marks: "))
+# subject_3 = float(input("enter a third subject marks: "))
+# subject_4 = float(input("enter a fourth subject marks: "))
+
+# total_marks = subject_1 + subject_2 + subject_3 + subject_4
+# print(f"Total marks of 4 subject is {total_marks}")
+
+# percentage = total_marks / 400 * 100
+# print(f"Total Percentage is {percentage}%")
+
+# if percentage >= 70 and percentage <= 100:
+#     print("Your Grade is Distinction")
+# elif percentage >= 60 and percentage < 70:
+#     print("Your Grade is first ")
+
+# elif percentage >= 40 and percentage < 60:
+#     print("Your Grade is pass")
+# elif percentage < 40:
+#     print("fail")
+
+######################################################################################################################################
+
+# question7
+################
+
+
+#####################################################################################################################################
+
+# question8
+###############
+
+# name = input("enter your name: ")
+# age = int(input("enter your age: "))
+# address = input("enter your address: ")
+
+# print(f"your name is {name}")
+# print(f"your age is {age}")
+# print(f"your address is {address}")
+
+
+#############################################################################################################################
+
+# question 9
+################
+
+# radius = float(input("Enter a radius: "))
+
+# area = 22/7 * radius**2
+
+# print(f"Area of a circle is equals to {area}")
+
+
+#######################################################################################################################
+
+# question 10
+##################
+
+
+#######################################################################################################################
+
+# question 11
+##################
+
+# num_1 = int(input("enter a first number: "))
+# num_2 = int(input("enter second number: "))
+# num_3 = int(input("enter third number: "))
+
+# if num_1 < num_2 and num_1 < num_3:
+#     print(f"smallest num among 3 input is {num_1}")
+
+# elif num_2 < num_1 and num_2 < num_3:
+#     print(f"smallest num among 3 input is  {num_2}")
+
+# else:
+
+#     print(f"smallest num among 3 input is  {num_3}")
+
+#######################################################################################################################
+
+# question 12
+#################
+
+# first_num = int(input("enter a first number: "))
+# sec_num = int(input("enter second number: "))
+# third_num = int(input("enter third number: "))
+
+# sum = first_num + sec_num + third_num
+# print(f"total sum is {sum}")
+
+
+#######################################################################################################################
+
+# question13
+################
+
+# base = float(input("Enter the length of base: "))
+
+# height = float(input("Enter the length of height: "))
+
+# area = 1/2 * base * height
+
+# print(f"The area of a right angled triangle is {area}")
+
+#############################################################################################################################
+
+# question14
+################
